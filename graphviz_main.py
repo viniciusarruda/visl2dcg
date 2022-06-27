fcaaec5e1a22155a781e8f9617669f3175e26a81
Vinicius Arruda <viniciusferracoarruda@gmail.com>
2022-06-26 23:27:53 -0300

    fix some stuff

--- graphviz_main.py
+++ graphviz_main.py
@@ -1,18 +1,79 @@
 import json
 import graphviz
+import os
 
 with open('formatted.json', 'r', encoding='utf-8') as f:
     visl_content = json.load(f)
 
 
-g = graphviz.Digraph('G', filename='hello.gv')
-g.edge('Hello', 'World')
-# g.view()
-graphviz.render('dot', 'png', 'spam.gv')
+def format_levels(tree):
 
+    formatted_tree = []
+    formatted_tree.append((0, 'N0', tree[0], False))
+    e = 1
+    for t in tree[1:]:
+        
+        level = 1
+        while t[0] == '=':
+            t = t[1:]
+            level += 1
+        
+        if '\t' in t:
+            ts = t.split('\t')
+            assert len(ts) == 2
+            formatted_tree.append((level, f'N{e}', ts[0], False))
+            e += 1
+            formatted_tree.append((level + 1, f'N{e}', ts[1], True))
+        else:
+            formatted_tree.append((level, f'N{e}', t, False))
+
+        e += 1
+
+    return formatted_tree
+
+a2s = []
+tn = 0
 for ext, tree_sentences in visl_content.items():
     for ts in tree_sentences:
         xs = list(ts.items())
         assert len(xs) == 1
         sentence, trees = xs[0]
-        # for ax, tree in trees.items():
+
+        if len(trees) > 0:
+
+            for ax, tree in trees.items():
+
+                g = graphviz.Digraph('G', filename=f'output/{tn}_{ax}.gv')
+                g.attr('node', shape='ellipse')
+                
+                try:
+                    tree = format_levels(tree)
+                except:
+                    print(sentence)
+                    print(tree)
+                    exit()
+                for t in tree:
+                    if t[3]: # is leaf
+                        g.attr('node', shape='plaintext')
+                        g.node(t[1], label=t[2].replace(':', '&#58;'))
+                        g.attr('node', shape='ellipse')
+                    else:
+                        g.node(t[1], label=t[2].replace(':', '&#58;'))
+
+                parent = []
+                for i in range(1, len(tree)):
+                    
+                    if tree[i][0] > tree[i-1][0]:
+                        parent.append(tree[i-1][1])
+                    elif tree[i][0] < tree[i-1][0]:
+                        parent.pop()
+
+                    g.edge(parent[-1], tree[i][1])
+
+                g.save()
+            
+            tn += 1
+
+
+for tn in os.listdir('output'):
+    graphviz.render('dot', 'png', f'output/{tn}')
--- main.py
+++ main.py
@@ -9,10 +9,37 @@ filename = 'Bosque_CF_8.0.ad.visl.txt'
 with open(filename, 'r', encoding='cp1252') as f:
     data = f.read()
 
+with open('Bosque_CF_8.0.ad.visl_utf8.txt', 'w', encoding='utf-8') as f:
+    f.write(data)
+
+
+vou ter que corrigir no arquivo msm.. tem jeito n
+corrigir o que eu criei no utf8? vai no mais facil.. se o prof n aprovar,
+ai vc deixa as correções aqui no codigomas isso é pessimo
+
+foco em fazer rapido e iterar para ver se é isso msm: 
+- coletar todas as arvores
+- interpretar elas de alguma forma, sem adentrar na linguistica, apenas para saber quando um item é diferente de outro
+- com isso, computar frequencia de regras
+- pronto, vc vai ter as estatisticas.. faz um sisteminha simples baseado em estatistica (frequencia)
+- da pra fazer um sistema de inferencia e testar.. 
+
+exit()
 data = data.strip()
 
 # This seems to be an error, need to edit, removing the <s> and </s>
 data = data.replace("\n</s>\n<s>\n&&\nA", "&&\nA")
+
+# Another formatting error
+data = data.replace(".\n\nSOURCE:", ".\n\n</s>\n</p>\n<p>\n<s>\n\nSOURCE:")
+
+# eu acabei bugando uma coisa, preciso baixar de novo
+# ai segue concertando via codigo msm..
+# confirmar se tem o numero correto de arvores..
+# depois que tera as arvores certas, comecar a interpretar o que esta querendo dizer aquilo
+# concertar aqui.. é melhor via codigo msm
+# ta faltando as quebras de s 
+
 
 visl_content = {}
 
